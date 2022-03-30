c8567f19e8866e69330729dfeda8a2405dc81020
ArtemCH240322 <sun2383@mail.ru>
2022-03-31 00:37:50 +0300

    Выполнил дз номер 1

--- src/Homwork2/Main.java
+++ src/Homwork2/Main.java
@@ -3,9 +3,40 @@ package Homwork2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("РџРёС€РµРј РЅР°С€Рµ РґР·!");
-        System.out.println("Р”РµР»Р°РµРј РєРѕРјРјРёС‚ Рё РїСѓС€");
-        System.out.println("РџРµСЂРµС…РѕРґРёРј РІ РіРёС‚С…Р°Р±");
+        // Задание 1
+        byte apple = 1;
+        short orange = 17;
+        int bar = 7;
+        long melon = 200L;
+        float salt = 1.5f;
+        double buckwheat = 2.8;
+        char paper = 35;
+        boolean bananaIsAudilf = bar > 5;
+
+
+        // Задание 2
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double totalWeightOfBoxers = boxer1 + boxer2;
+        double weightDifferenceBetweenBoxers = boxer2 - boxer1;
+        System.out.println("Общий вес боксеров " + totalWeightOfBoxers);
+        System.out.println("Разница в весе боксеров " + weightDifferenceBetweenBoxers);
+
+        // Задача 3
+        byte banana = 80;
+        short milk = 210;
+        short iceСream = 200;
+        short aggs = 280;
+        int mixAll = banana + milk + iceСream + aggs;
+        System.out.println("Смешать все в блендере " + mixAll + " грамм");
+
+
+
+
+
+
+
+
 
     }
 }
