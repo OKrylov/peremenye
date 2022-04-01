a0713fcae91d46cc03363d1a59c09c423790cd1d
OKrylov <okrylov@axcient.com>
2022-04-01 14:07:42 +0300

    Changed encoding to utf-8

--- src/Homwork2/Main.java
+++ src/Homwork2/Main.java
@@ -3,7 +3,7 @@ package Homwork2;
 public class Main {
 
     public static void main(String[] args) {
-        // Задание 1
+        // Р—Р°РґР°РЅРёРµ 1
         byte apple = 1;
         short orange = 17;
         int bar = 7;
@@ -14,100 +14,100 @@ public class Main {
         boolean bananaIsAudilf = bar > 5;
 
 
-        // Задание 2
+        // Р—Р°РґР°РЅРёРµ 2
         double boxer1 = 78.2;
         double boxer2 = 82.7;
         double totalWeightOfBoxers = boxer1 + boxer2;
         double weightDifferenceBetweenBoxers = boxer2 - boxer1;
-        System.out.println("Общий вес боксеров " + totalWeightOfBoxers);
-        System.out.println("Разница в весе боксеров " + weightDifferenceBetweenBoxers);
+        System.out.println("РћР±С‰РёР№ РІРµСЃ Р±РѕРєСЃРµСЂРѕРІ " + totalWeightOfBoxers);
+        System.out.println("Р Р°Р·РЅРёС†Р° РІ РІРµСЃРµ Р±РѕРєСЃРµСЂРѕРІ " + weightDifferenceBetweenBoxers);
 
-        // Задача 3
+        // Р—Р°РґР°С‡Р° 3
         byte banana = 5;
         byte gramm = 80;
         int weight = banana * gramm;
-        System.out.println("Вес бананов " + weight + " грамм");
+        System.out.println("Р’РµСЃ Р±Р°РЅР°РЅРѕРІ " + weight + " РіСЂР°РјРј");
 
         // milk
         short allmilkMl = 200;
         short milkMl = 100;
         short milkGm = 105;
         int milkThings = allmilkMl / milkMl;
-        System.out.println("Количество бутылок молока " + milkThings + " шт");
+        System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ Р±СѓС‚С‹Р»РѕРє РјРѕР»РѕРєР° " + milkThings + " С€С‚");
         short milkTheings = 2;
         int allmilkGr = milkGm * milkThings;
-        System.out.println("Количество молока " + allmilkGr + " Грамм");
+        System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ РјРѕР»РѕРєР° " + allmilkGr + " Р“СЂР°РјРј");
 
         // eggs
         byte eggs = 4;
         short eggsGm = 70;
         int allEggsGm = eggs * eggsGm;
-        System.out.println("Вес яиц " + allEggsGm + " грамм");
+        System.out.println("Р’РµСЃ СЏРёС† " + allEggsGm + " РіСЂР°РјРј");
 
 
         byte allBanana = 80;
         short milk = 210;
-        short iceСream = 200;
+        short iceРЎream = 200;
         short aggs = 280;
-        int mixAll = allBanana + milk + iceСream + aggs;
-        System.out.println("Смешать все в блендере " + mixAll + " грамм");
+        int mixAll = allBanana + milk + iceРЎream + aggs;
+        System.out.println("РЎРјРµС€Р°С‚СЊ РІСЃРµ РІ Р±Р»РµРЅРґРµСЂРµ " + mixAll + " РіСЂР°РјРј");
         short allProducts = 770;
         float kilogram = 1000L;
         float inKilogram = kilogram / allProducts;
-        System.out.println("Вес продуктов " + inKilogram + " в кг.");
+        System.out.println("Р’РµСЃ РїСЂРѕРґСѓРєС‚РѕРІ " + inKilogram + " РІ РєРі.");
 
 
-        // Зфдача 4
+        // Р—С„РґР°С‡Р° 4
         byte loseWeight = 7;
-        short trainerРrogram1 = 250;
-        short trainerРrogram2 = 500;
+        short trainerР rogram1 = 250;
+        short trainerР rogram2 = 500;
         float KilogramsInGrams = 1000L;
         float loseWeightInGrams = loseWeight * KilogramsInGrams;
-        System.out.println("Количество грамм " + loseWeightInGrams + " в кг");
-        float daysTrainerРrogram1 = loseWeightInGrams / trainerРrogram1;
-        float daysTrainerРrogram2 = loseWeightInGrams / trainerРrogram2;
-        float average = (trainerРrogram1 + trainerРrogram2) / 2;
+        System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ РіСЂР°РјРј " + loseWeightInGrams + " РІ РєРі");
+        float daysTrainerР rogram1 = loseWeightInGrams / trainerР rogram1;
+        float daysTrainerР rogram2 = loseWeightInGrams / trainerР rogram2;
+        float average = (trainerР rogram1 + trainerР rogram2) / 2;
         float averageDey = loseWeightInGrams / average;
-        System.out.println("Диета номер 1- " + daysTrainerРrogram1 + " дней");
-        System.out.println("Диета номер 2- " + daysTrainerРrogram2 + " дней");
-        System.out.println("Срендее значение " + average + " грамм");
-        System.out.println("Среднее количество дней " + averageDey + " дней" );
+        System.out.println("Р”РёРµС‚Р° РЅРѕРјРµСЂ 1- " + daysTrainerР rogram1 + " РґРЅРµР№");
+        System.out.println("Р”РёРµС‚Р° РЅРѕРјРµСЂ 2- " + daysTrainerР rogram2 + " РґРЅРµР№");
+        System.out.println("РЎСЂРµРЅРґРµРµ Р·РЅР°С‡РµРЅРёРµ " + average + " РіСЂР°РјРј");
+        System.out.println("РЎСЂРµРґРЅРµРµ РєРѕР»РёС‡РµСЃС‚РІРѕ РґРЅРµР№ " + averageDey + " РґРЅРµР№" );
 
-        // Задача 5
+        // Р—Р°РґР°С‡Р° 5
         int Masha = 67760;
         byte increaseMasha = 10;
-        int annualIncomeВeforeMasha = Masha * 12;
-        System.out.println("Годовой доход Маши " + annualIncomeВeforeMasha + " руб");
+        int annualIncomeР’eforeMasha = Masha * 12;
+        System.out.println("Р“РѕРґРѕРІРѕР№ РґРѕС…РѕРґ РњР°С€Рё " + annualIncomeР’eforeMasha + " СЂСѓР±");
         float withIncreaseMasha = Masha * 0.10f + Masha;
-        System.out.println("Доход Маши с прибавкой " + withIncreaseMasha + " руб");
+        System.out.println("Р”РѕС…РѕРґ РњР°С€Рё СЃ РїСЂРёР±Р°РІРєРѕР№ " + withIncreaseMasha + " СЂСѓР±");
         float allWithMasha = withIncreaseMasha * 12f;
-        System.out.println("Годовой доход Маши после повышения " + allWithMasha + " руб");
-        float differenceInAnnualIncomeMasha = allWithMasha - annualIncomeВeforeMasha;
-        System.out.println("Разница годовых доходов Маши " + differenceInAnnualIncomeMasha + " руб");
+        System.out.println("Р“РѕРґРѕРІРѕР№ РґРѕС…РѕРґ РњР°С€Рё РїРѕСЃР»Рµ РїРѕРІС‹С€РµРЅРёСЏ " + allWithMasha + " СЂСѓР±");
+        float differenceInAnnualIncomeMasha = allWithMasha - annualIncomeР’eforeMasha;
+        System.out.println("Р Р°Р·РЅРёС†Р° РіРѕРґРѕРІС‹С… РґРѕС…РѕРґРѕРІ РњР°С€Рё " + differenceInAnnualIncomeMasha + " СЂСѓР±");
 
 
         int Denis = 83690;
         byte increaseDenis = 10;
-        int annualIncomeВeforeDenis = Denis * 12;
-        System.out.println("Годовой доход Дениса " + annualIncomeВeforeDenis + " руб");
+        int annualIncomeР’eforeDenis = Denis * 12;
+        System.out.println("Р“РѕРґРѕРІРѕР№ РґРѕС…РѕРґ Р”РµРЅРёСЃР° " + annualIncomeР’eforeDenis + " СЂСѓР±");
         float withIncreaseDenis = Denis * 0.10f + Denis;
-        System.out.println("Доход Дениса с прибавкой " + withIncreaseDenis + " руб");
+        System.out.println("Р”РѕС…РѕРґ Р”РµРЅРёСЃР° СЃ РїСЂРёР±Р°РІРєРѕР№ " + withIncreaseDenis + " СЂСѓР±");
         float allWithDenis = withIncreaseDenis * 12f;
-        System.out.println("Годовой доход Дениса после повышения " + allWithDenis + " руб");
-        float differenceInAnnualIncomeDenis = allWithDenis - annualIncomeВeforeDenis;
-        System.out.println("Разница годовых доходов Дениса " + differenceInAnnualIncomeDenis + " руб");
+        System.out.println("Р“РѕРґРѕРІРѕР№ РґРѕС…РѕРґ Р”РµРЅРёСЃР° РїРѕСЃР»Рµ РїРѕРІС‹С€РµРЅРёСЏ " + allWithDenis + " СЂСѓР±");
+        float differenceInAnnualIncomeDenis = allWithDenis - annualIncomeР’eforeDenis;
+        System.out.println("Р Р°Р·РЅРёС†Р° РіРѕРґРѕРІС‹С… РґРѕС…РѕРґРѕРІ Р”РµРЅРёСЃР° " + differenceInAnnualIncomeDenis + " СЂСѓР±");
 
 
         int Kris = 76230;
         byte increaseKris = 10;
-        int annualIncomeВeforeKris = Kris * 12;
-        System.out.println("Годовой доход Кристины " + annualIncomeВeforeKris + " руб");
+        int annualIncomeР’eforeKris = Kris * 12;
+        System.out.println("Р“РѕРґРѕРІРѕР№ РґРѕС…РѕРґ РљСЂРёСЃС‚РёРЅС‹ " + annualIncomeР’eforeKris + " СЂСѓР±");
         float withIncreaseKris = Kris * 0.10f + Kris;
-        System.out.println("Доход Кристины с прибавкой " + withIncreaseKris + " руб");
+        System.out.println("Р”РѕС…РѕРґ РљСЂРёСЃС‚РёРЅС‹ СЃ РїСЂРёР±Р°РІРєРѕР№ " + withIncreaseKris + " СЂСѓР±");
         float allWithKris = withIncreaseKris * 12f;
-        System.out.println("Годовой доход Кристины после повышения " + allWithKris + " руб");
-        float differenceInAnnualIncomeKris = allWithKris - annualIncomeВeforeKris;
-        System.out.println("Разница годовых доходов Кристины " + differenceInAnnualIncomeKris + " руб");
+        System.out.println("Р“РѕРґРѕРІРѕР№ РґРѕС…РѕРґ РљСЂРёСЃС‚РёРЅС‹ РїРѕСЃР»Рµ РїРѕРІС‹С€РµРЅРёСЏ " + allWithKris + " СЂСѓР±");
+        float differenceInAnnualIncomeKris = allWithKris - annualIncomeР’eforeKris;
+        System.out.println("Р Р°Р·РЅРёС†Р° РіРѕРґРѕРІС‹С… РґРѕС…РѕРґРѕРІ РљСЂРёСЃС‚РёРЅС‹ " + differenceInAnnualIncomeKris + " СЂСѓР±");
 
 
 
